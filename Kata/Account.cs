6c00e4683db8b9e7583279509e63884171de36e4
minamba camara <minamba.c@gmail.com>
2019-10-23 20:58:03 +0200

    premier commit du kata

---
+++ Kata/Account.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+    public class Account
+    {
+        /////////////////Properties///////////////////////////
+        public int _deposit { get; set; }
+        public int _withDraw { get; set; }
+        private static int balance = 500;
+
+        ////////////////////Methods///////////////////////////
+        public int Deposit(int d)
+        {
+            d = _deposit;
+            balance = balance + d; 
+            return d;
+        }
+
+        public int WithDraw(int wd)
+        {
+            wd = _withDraw;
+            balance = balance - wd;
+            return wd;
+        }
+
+
+        public string PrintStatement(string choice, int amount)
+        {
+            string day = System.DateTime.Now.Day.ToString();
+            string month = System.DateTime.Now.Month.ToString();
+            string year = System.DateTime.Now.Year.ToString();
+            string date = day + "/" + month + "/" + year;
+            string statement="";
+            _deposit = amount;
+            _withDraw = amount;
+
+            switch (choice)
+            {
+                case "D":
+                        statement = date + " " + "+" + Deposit(amount) + " " + balance.ToString();
+                        return statement;
+
+                case "R":
+                        statement = date + " " + "-" + WithDraw(amount) + " " + balance.ToString();
+                        return statement;
+            }
+
+            return statement;
+        }
+    }
+}
---
+++ Kata/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kata
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Sectionnez votre opération :");
+                Console.WriteLine("'D' pour faire un dépot | 'R' pour faire un retrait");
+                var choice = Console.ReadLine().ToUpper();
+                Console.WriteLine("Veuillez saisir un montant :");
+                int amount = int.Parse(Console.ReadLine());
+
+                Account _account = new Account();
+                Console.WriteLine(_account.PrintStatement(choice, amount));
+                Console.ReadLine();
+            }
+        }
+    }
+}
